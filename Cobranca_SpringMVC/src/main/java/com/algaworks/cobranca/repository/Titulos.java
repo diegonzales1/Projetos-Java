7139398a20afa0bcd3176607f2edfa501f3d5801
Diego Gonzales <diegogonzales@outlook.com.br>
2021-06-03 20:08:05 -0300

    Add Cobranca MVC

---
+++ Cobranca_SpringMVC/src/main/java/com/algaworks/cobranca/repository/Titulos.java
@@ -0,0 +1,11 @@
+package com.algaworks.cobranca.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.algaworks.cobranca.modal.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+	public List<Titulo> findByDescricaoContaining(String descricao);
+}
