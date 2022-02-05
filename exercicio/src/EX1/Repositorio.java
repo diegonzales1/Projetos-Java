f4c0bab53a0b4c2691ac1ee82e2ddaf78484df1d
Diego Gonzales de Andrade <61495537+diegonzales1@users.noreply.github.com>
2022-02-05 11:36:36 -0300

    Delete exercicio directory

--- exercicio/src/EX1/Repositorio.java
+++
@@ -1,6 +0,0 @@
-package EX1;
-
-public interface Repositorio {
-
-	public void cadastrarCliente(Cliente cliente);
-}
--- exercicio/src/EX2/Repositorio.java
+++
@@ -1,8 +0,0 @@
-package EX2;
-
-public interface Repositorio {
-
-	public void adicionaLivro(Livro livro);
-
-	public void buscarLivro(String titulo);
-}
